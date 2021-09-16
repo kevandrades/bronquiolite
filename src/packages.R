ec6f93d2700ed3157e3e59345c06ffb52311aaf5
kevandrades <kevsdeandrade@gmail.com>
2021-09-16 17:37:34 -0300

    define function to generate table of tests

--- src/packages.R
+++ src/packages.R
@@ -1,5 +1,22 @@
 if (!require(pacman)) install.packages('pacman')
 
-pacman::p_load(tidyverse, magrittr, readxl, purrr, installr, tufte)
+pacman::p_load(
+    tidyverse, magrittr, readxl,
+    purrr, installr, tufte,
+    BiocManager,
+    knitr, kableExtra
+)
 
 if (!(rmarkdown::pandoc_available())) install.pandoc()
+
+table_from_test <- function(test) {
+    data.frame(
+        p.value = c(test$p.value %>% round(3)), 
+        result = c(if (test$p.value > 0.05) "Aceita H0" else "Rejeita H0")
+    ) %>%
+    kable(
+        col.names = c("P-valor", "Conclusão"),
+        escape = T, caption = test$data %>% str_replace("by", "por")
+    ) %>%
+  return()
+}
