e4b0aaa50fce813e5b52b2411d8dcd3b04f1bf03
kevandrades <kevsdeandrade@gmail.com>
2021-09-16 16:59:08 -0300

    update packages

--- src/packages.R
+++ src/packages.R
@@ -1,5 +1,5 @@
 if (!require(pacman)) install.packages('pacman')
 
-pacman::p_load(tidyverse, magrittr, readxl, purrr, installr)
+pacman::p_load(tidyverse, magrittr, readxl, purrr, installr, tufte)
 
 if (!(rmarkdown::pandoc_available())) install.pandoc()
