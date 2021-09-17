4c9ab2a09ec775318d6eeaee53e72dec6782f2c0
kevandrades <kevsdeandrade@gmail.com>
2021-09-17 06:49:32 -0300

    formatting, add rmdformats

--- src/packages.R
+++ src/packages.R
@@ -1,9 +1,9 @@
-if (!require(pacman)) install.packages('pacman')
+if (!require(pacman)) install.packages("pacman")
 
 pacman::p_load(
     tidyverse, magrittr, readxl,
     purrr, installr, tufte,
-    BiocManager,
+    BiocManager, rmdformats,
     knitr, kableExtra
 )
 
@@ -11,12 +11,12 @@ if (!(rmarkdown::pandoc_available())) install.pandoc()
 
 table_from_test <- function(test) {
     data.frame(
-        p.value = c(test$p.value %>% round(3)), 
+        p.value = c(test$p.value %>% round(3)),
         result = c(if (test$p.value > 0.05) "Aceita H0" else "Rejeita H0")
     ) %>%
-    kable(
-        col.names = c("P-valor", "Conclusão"),
-        escape = T, caption = test$data %>% str_replace("by", "por")
-    ) %>%
-  return()
+        kable(
+            col.names = c("P-valor", "Conclusão"),
+            escape = T, caption = test$data %>% str_replace("by", "por")
+        ) %>%
+        return()
 }
